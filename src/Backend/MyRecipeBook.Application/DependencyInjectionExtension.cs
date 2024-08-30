da8b05ca1b07fd27d6c2abff57c83139c0ddabb8
Caio Robert <caio.robert@live.com>
2024-08-29 23:00:36 -0300

    feat: adding all code without separate in short pull requests

---
+++ src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Application.Services.AutoMapper;
+using MyRecipeBook.Application.Services.Cryptography;
+using MyRecipeBook.Application.UseCases.User.Register;
+
+namespace MyRecipeBook.Application
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddPasswordEcrypter(services, configuration);
+            AddAutoMapper(services);
+            AddUseCases(services);
+        }
+
+        private static void AddAutoMapper(IServiceCollection services )
+        {
+            services.AddScoped(option => new AutoMapper.MapperConfiguration(option =>
+            {
+                option.AddProfile(new AutoMapping());
+            }).CreateMapper());
+        }
+
+        private static void AddUseCases(IServiceCollection services)
+        {
+            services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+        }
+
+        private static void AddPasswordEcrypter(IServiceCollection services, IConfiguration configuration)
+        {
+            var additionalKey = configuration.GetValue<string>("Settings:Password:AdditionalKey");
+
+            services.AddScoped(options => new PasswordEcripter(additionalKey!));
+        }
+    }
+}
---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exceptions;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesExceptions.NAME_EMPTY);
+            RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessagesExceptions.EMAIL_EMPTY);
+            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesExceptions.EMAIL_INVALID);
+            RuleFor(user => user.Password.Length).GreaterThan(6).WithMessage(ResourceMessagesExceptions.PASSWORD_EMPTY);
+        }
+    }
+}
