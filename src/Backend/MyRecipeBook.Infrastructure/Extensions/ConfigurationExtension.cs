f5b6f7b0d27c7b149bb0d2ac9ffc3a82510388a3
Caio Robert <caio.robert@live.com>
2024-09-03 20:53:23 -0300

    feat: Add migration to add schema meulivrodereceitas
    #3

---
+++ src/Backend/MyRecipeBook.Infrastructure/Extensions/ConfigurationExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using MyRecipeBook.Domain.Enums;
+
+namespace MyRecipeBook.Infrastructure.Extensions
+{
+    public static class ConfigurationExtension
+    {
+        public static DatabaseType DatabaseType(this IConfiguration configuration)
+        {
+            var databaseType = configuration.GetConnectionString("DatabaseType");
+
+            return (DatabaseType)Enum.Parse(typeof(DatabaseType), databaseType!);
+        }
+
+        public static string ConnectionString(this IConfiguration configuration)
+        {
+            var dataseType = configuration.DatabaseType();
+
+            if (dataseType == Domain.Enums.DatabaseType.SqlServer)
+                return configuration.GetConnectionString("connectionSQLServer")!;
+            else
+                return configuration.GetConnectionString("connectionOtherDB")!;
+        }
+    }
+}
