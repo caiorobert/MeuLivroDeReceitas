4af95549dcc4f609c41fa708800a1ce677d81571
Caio Robert <caio.robert@live.com>
2024-10-15 22:15:07 -0300

    test: testes de UseCases iniciais

---
+++ tests/CommonTestUtilities/Cryptography/PasswordEcripterBuilder.cs
@@ -0,0 +1,9 @@
+﻿using MyRecipeBook.Application.Services.Cryptography;
+
+namespace CommonTestUtilities.Cryptography
+{
+    public class PasswordEcripterBuilder
+    {
+        public static PasswordEcripter Build() => new PasswordEcripter("abc1234");
+    }
+}
