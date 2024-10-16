39474b1c79484c418df8832764346781ff260ff2
Caio Robert <caio.robert@live.com>
2024-10-15 22:14:23 -0300

    cleanup: renomeando variáveis

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -11,23 +11,23 @@ namespace MyRecipeBook.Application.UseCases.User.Register
 {
     public class RegisterUserUseCase : IRegisterUserUseCase
     {
-        private readonly IUserWriteOnlyRepository _userWriteOnlyRepository;
-        private readonly IUserReadOnlyRepository _userReadOnlyRepository;
+        private readonly IUserWriteOnlyRepository _writeOnlyRepository;
+        private readonly IUserReadOnlyRepository _readOnlyRepository;
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly PasswordEcripter _passwordEcripter;
 
         public RegisterUserUseCase
         (
-            IUserWriteOnlyRepository userWriteOnlyRepository,
-            IUserReadOnlyRepository userReadOnlyRepository,
+            IUserWriteOnlyRepository writeOnlyRepository,
+            IUserReadOnlyRepository readOnlyRepository,
             IUnitOfWork unitOfWork,
             IMapper mapper,
             PasswordEcripter passwordEcripter
         )
         {
-            _userWriteOnlyRepository = userWriteOnlyRepository;
-            _userReadOnlyRepository = userReadOnlyRepository;
+            _writeOnlyRepository = writeOnlyRepository;
+            _readOnlyRepository = readOnlyRepository;
             _unitOfWork = unitOfWork;
             _mapper = mapper;
             _passwordEcripter = passwordEcripter;
@@ -38,16 +38,15 @@ namespace MyRecipeBook.Application.UseCases.User.Register
             await Validate(request);
 
             var user = _mapper.Map<Domain.Entities.User>(request);
-
             user.Password = _passwordEcripter.Encrypt(request.Password);
 
-            await _userWriteOnlyRepository.Add(user);
+            await _writeOnlyRepository.Add(user);
 
             await _unitOfWork.Commit();
 
             return new ResponseRegisteredUserJson
             {
-                Name = request.Name,
+                Name = user.Name,
             };
         }
 
@@ -57,16 +56,16 @@ namespace MyRecipeBook.Application.UseCases.User.Register
 
             var result = validator.Validate(request);
 
-            var emailExist = await _userReadOnlyRepository.ExistActiveUserWithEmail(request.Email);
+            var emailExist = await _readOnlyRepository.ExistActiveUserWithEmail(request.Email);
 
             if (emailExist)
                 result.Errors.Add(new FluentValidation.Results.ValidationFailure(string.Empty, ResourceMessagesExceptions.EMAIL_ALREADY_REGISTERED));
 
             if (result.IsValid == false)
             {
-                var errorMessages = result.Errors.Select(e => e.ErrorMessage);
+                var errorMessages = result.Errors.Select(e => e.ErrorMessage).ToList();
 
-                throw new ErrorOnValidationException(errorMessages.ToList());
+                throw new ErrorOnValidationException(errorMessages);
             }
         }
     }
