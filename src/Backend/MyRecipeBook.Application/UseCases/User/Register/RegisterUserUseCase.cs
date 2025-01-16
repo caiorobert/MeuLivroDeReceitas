e5810ba153f945f06f150ceee1729e9c8d09c465
Caio Robert <caio.robert@live.com>
2025-01-16 20:20:13 -0300

    fix: ajustando código de acordo com métricas sonarcloud

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -2,6 +2,7 @@
 using MyRecipeBook.Application.Services.Cryptography;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Extensions;
 using MyRecipeBook.Domain.Repositories;
 using MyRecipeBook.Domain.Repositories.User;
 using MyRecipeBook.Exceptions;
@@ -61,7 +62,7 @@ namespace MyRecipeBook.Application.UseCases.User.Register
             if (emailExist)
                 result.Errors.Add(new FluentValidation.Results.ValidationFailure(string.Empty, ResourceMessagesExceptions.EMAIL_ALREADY_REGISTERED));
 
-            if (result.IsValid == false)
+            if (result.IsValid.IsFalse())
             {
                 var errorMessages = result.Errors.Select(e => e.ErrorMessage).ToList();
 
