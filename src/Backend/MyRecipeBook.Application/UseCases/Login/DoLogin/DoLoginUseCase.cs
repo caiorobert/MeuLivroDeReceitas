c63f2d8a1cd902d4ecb4e7ad84820f075c9dcd5c
Caio Robert <caio.robert@live.com>
2025-01-21 21:17:06 -0300

    feat: regra de negócio para fazer login
    #22

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Login/DoLogin/DoLoginUseCase.cs
@@ -0,0 +1,45 @@
+﻿using MyRecipeBook.Application.Services.Cryptography;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Extensions;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+
+namespace MyRecipeBook.Application.UseCases.Login.DoLogin
+{
+    public class DoLoginUseCase : IDoLoginUseCase
+    {
+        private readonly IUserReadOnlyRepository _repository;
+        private readonly PasswordEcripter _passwordEcripter;
+
+        public DoLoginUseCase(IUserReadOnlyRepository repository, PasswordEcripter passwordEcripter)
+        {
+            _repository = repository;
+            _passwordEcripter = passwordEcripter;
+        }
+
+        public async Task<ResponseRegisteredUserJson> Execute(RequestLoginJson request)
+        {
+            Validate(request);
+
+            var encriptedPassword = _passwordEcripter.Encrypt(request.Password);
+
+            var user = await _repository.GetByEmailAndPassword(request.Email, encriptedPassword) ?? throw new InvalidLoginException();
+
+            return new ResponseRegisteredUserJson
+            {
+                Name = user.Name
+            };
+        }
+
+        private static void Validate(RequestLoginJson request)
+        {
+            var validator = new DoLoginValidator();
+
+            var result = validator.Validate(request);
+
+            if (result.IsValid.IsFalse())
+                throw new UnauthorizedAccessException();
+        }
+    }
+}
---
+++ src/Backend/MyRecipeBook.Application/UseCases/Login/DoLogin/DoLoginValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Domain.Extensions;
+
+namespace MyRecipeBook.Application.UseCases.Login.DoLogin
+{
+    public class DoLoginValidator : AbstractValidator<RequestLoginJson>
+    {
+        public DoLoginValidator()
+        {
+            RuleFor(user => user.Email).NotEmpty();
+            RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6);
+            When(user => user.Email.NotEmpty(), () =>
+            {
+                RuleFor(user => user.Email).EmailAddress();
+            });
+        }
+    }
+}
