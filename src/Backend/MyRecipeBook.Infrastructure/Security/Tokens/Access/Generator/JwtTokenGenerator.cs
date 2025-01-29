48e9da797656519b47b4617813c4f7962177b617
Caio Robert <caio.robert@live.com>
2025-01-29 20:15:01 -0300

    feat: geração de access token em JWT para autenticação

---
+++ src/Backend/MyRecipeBook.Infrastructure/Security/Tokens/Access/Generator/JwtTokenGenerator.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using MyRecipeBook.Domain.Security.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MyRecipeBook.Infrastructure.Security.Tokens.Access.Generator
+{
+    public class JwtTokenGenerator : IAccessTokenGenerator
+    {
+        private readonly uint _expirationTimeMinutes;
+        private readonly string _singleKey;
+        public JwtTokenGenerator(uint expirationTimeMinutes, string singleKey)
+        {
+            _expirationTimeMinutes = expirationTimeMinutes;
+            _singleKey = singleKey;
+        }
+
+        public string Generate(Guid userIdentifier)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Sid, userIdentifier.ToString())
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(_expirationTimeMinutes),
+                SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(securityToken);
+        }
+
+        private SymmetricSecurityKey SecurityKey()
+        {
+            var bytes = Encoding.UTF8.GetBytes(_singleKey);
+
+            return new SymmetricSecurityKey(bytes);
+        }
+    }
+}
