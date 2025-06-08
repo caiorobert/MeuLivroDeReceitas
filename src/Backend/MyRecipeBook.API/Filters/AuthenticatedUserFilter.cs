afcadd3f3e2ab4bcefa1a63532f9ccabbb07072b
Caio Robert <caio.robert@live.com>
2025-06-08 17:15:00 -0300

    feat: recuperando entidade user por token e perfil do user na autenticação

--- src/Backend/MyRecipeBook.API/Filters/AuthenticatedUserFilter.cs
+++ src/Backend/MyRecipeBook.API/Filters/AuthenticatedUserFilter.cs
@@ -56,7 +56,7 @@ namespace MyRecipeBook.API.Filters
         private static string TokenOnRequest(AuthorizationFilterContext context)
         {
             var authentication = context.HttpContext.Request.Headers.Authorization.ToString();
-            if (!string.IsNullOrEmpty(authentication))
+            if (string.IsNullOrWhiteSpace(authentication))
             {
                 throw new MyRecipeBookException(ResourceMessagesExceptions.NO_TOKEN);
             }
---
+++ src/Backend/MyRecipeBook.Infrastructure/Services/LoggedUser/LoggedUser.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Security.Tokens;
+using MyRecipeBook.Domain.Services.LoggedUser;
+using MyRecipeBook.Infrastructure.DataAccess;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MyRecipeBook.Infrastructure.Services.LoggedUser
+{
+    internal class LoggedUser : ILoggedUser
+    {
+        public readonly MyRecipeBookDbContext _dbcontext;
+        private readonly ITokenProvider _tokenProvider;
+
+        public LoggedUser(MyRecipeBookDbContext dbContext, ITokenProvider tokenProvider)
+        {
+            _dbcontext = dbContext;
+            _tokenProvider = tokenProvider;
+        }
+
+        public async Task<User> User()
+        {
+            var token = _tokenProvider.Value();
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var jwtSecurityToken = tokenHandler.ReadJwtToken(token);
+
+            var identifier = jwtSecurityToken.Claims.First(c => c.Type == ClaimTypes.Sid).Value;
+
+            var userIdentifier = Guid.Parse(identifier);
+
+            return await _dbcontext
+                .Users
+                .AsNoTracking()
+                .FirstAsync(user => user.Active && user.UserIdentifier == userIdentifier);
+        }
+    }
+}
