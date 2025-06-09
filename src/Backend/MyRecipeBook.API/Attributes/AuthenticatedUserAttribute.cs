47e901552e6aa85e16f931473c66ebe3003f0d8b
Caio Robert <caio.robert@live.com>
2025-06-09 01:04:31 -0300

    refactor: attributeUssage to SonarQube

--- src/Backend/MyRecipeBook.API/Attributes/AuthenticatedUserAttribute.cs
+++ src/Backend/MyRecipeBook.API/Attributes/AuthenticatedUserAttribute.cs
@@ -3,6 +3,7 @@ using MyRecipeBook.API.Filters;
 
 namespace MyRecipeBook.API.Attributes
 {
+    [AttributeUsage(AttributeTargets.Method)]
     public class AuthenticatedUserAttribute : TypeFilterAttribute
     {
         public AuthenticatedUserAttribute() : base(typeof(AuthenticatedUserFilter))
