f960aa2efb2810ea1dd99e5ac05571346689d3b3
Caio Robert <caio.robert@live.com>
2025-06-10 21:58:00 -0300

    feat: Adicionando validator de receitas

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipe/RecipeValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exceptions;
+
+namespace MyRecipeBook.Application.UseCases.Recipe
+{
+    public class RecipeValidator : AbstractValidator<RequestRecipeJson>
+    {
+        public RecipeValidator()
+        {
+            RuleFor(recipe => recipe.Title).NotEmpty().WithMessage(ResourceMessagesExceptions.RECIPE_TITLE_EMPTY);
+            RuleFor(recipe => recipe.CookingTime).IsInEnum().WithMessage(ResourceMessagesExceptions.COOKING_TIME_NOT_SUPPORTED);
+            RuleFor(recipe => recipe.Difficulty).IsInEnum().WithMessage(ResourceMessagesExceptions.DIFFICULTY_LEVEL_NOT_SUPPORTED);
+        }
+    }
+}
