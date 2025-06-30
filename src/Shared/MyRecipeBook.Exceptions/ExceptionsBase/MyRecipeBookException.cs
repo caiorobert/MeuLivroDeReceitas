af4883aa4b9cbd5e78f70556eb81bd8fbf7ac9ac
Caio Robert <caio.robert@live.com>
2025-06-29 22:07:05 -0300

    feat(MyRecipeBook.Exceptions): adicionando todas as exceções

--- src/Shared/MyRecipeBook.Exceptions/ExceptionsBase/MyRecipeBookException.cs
+++ src/Shared/MyRecipeBook.Exceptions/ExceptionsBase/MyRecipeBookException.cs
@@ -1,7 +1,10 @@
-﻿namespace MyRecipeBook.Exceptions.ExceptionsBase
+﻿using System.Net;
+
+namespace MyRecipeBook.Exceptions.ExceptionsBase;
+public abstract class MyRecipeBookException : SystemException
 {
-    public class MyRecipeBookException : SystemException
-    {
-        public MyRecipeBookException(string message) : base(message) { }
-    }
+    protected MyRecipeBookException(string message) : base(message) { }
+
+    public abstract IList<string> GetErrorMessages();
+    public abstract HttpStatusCode GetStatusCode();
 }
--- src/Shared/MyRecipeBook.Exceptions/ExceptionsBase/RefreshTokenNotFoundException.cs
+++ src/Shared/MyRecipeBook.Exceptions/ExceptionsBase/RefreshTokenNotFoundException.cs
@@ -1,10 +1,9 @@
 ﻿using System.Net;
 
 namespace MyRecipeBook.Exceptions.ExceptionsBase;
-
-public class InvalidLoginException : MyRecipeBookException
+public class RefreshTokenNotFoundException : MyRecipeBookException
 {
-    public InvalidLoginException() : base(ResourceMessagesException.EMAIL_OR_PASSWORD_INVALID)
+    public RefreshTokenNotFoundException() : base(ResourceMessagesException.EXPIRED_SESSION)
     {
     }
 
