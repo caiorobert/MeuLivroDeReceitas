d96561ca772bf8a4e0d3be72370cc18c2703bc65
Caio Robert <caio.robert@live.com>
2025-06-29 21:22:17 -0300

    feat: Novos validators de recipe

--- src/Shared/MyRecipeBook.Communication/Requests/RequestRecipeJson.cs
+++ src/Shared/MyRecipeBook.Communication/Requests/RequestRecipeJson.cs
@@ -7,6 +7,7 @@ namespace MyRecipeBook.Communication.Requests
         public string Title { get; set; } = string.Empty;
         public CookingTime? CookingTime { get; set; }
         public Difficulty? Difficulty { get; set; }
+        public IList<string> Ingredients { get; set; } = [];
         public IList<RequestInstructionJson> Instructions { get; set; } = [];
         public IList<DishType> DishTypes { get; set; } = [];
     }
