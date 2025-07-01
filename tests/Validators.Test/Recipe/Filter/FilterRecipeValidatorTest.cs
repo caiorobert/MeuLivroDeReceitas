9d659aa0f20b4a60d1b9ca67da2562bbcaad5060
Caio Robert <caio.robert@live.com>
2025-07-01 20:27:13 -0300

    test: testes para filtro de receita

---
+++ tests/Validators.Test/Recipe/Filter/FilterRecipeValidatorTest.cs
@@ -0,0 +1,71 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.Recipe.Filter;
+using MyRecipeBook.Exceptions;
+using Shouldly;
+
+namespace Validators.Test.Recipe.Filter;
+public class FilterRecipeValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new FilterRecipeValidator();
+
+        var request = RequestFilterRecipeJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Error_Invalid_Cooking_Time()
+    {
+        var validator = new FilterRecipeValidator();
+
+        var request = RequestFilterRecipeJsonBuilder.Build();
+        request.CookingTimes.Add((MyRecipeBook.Communication.Enums.CookingTime)1000);
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(
+            e => e.ShouldHaveSingleItem(),
+            e => e.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesExceptions.COOKING_TIME_NOT_SUPPORTED))
+            );
+    }
+
+    [Fact]
+    public void Error_Invalid_Difficulty()
+    {
+        var validator = new FilterRecipeValidator();
+
+        var request = RequestFilterRecipeJsonBuilder.Build();
+        request.Difficulties.Add((MyRecipeBook.Communication.Enums.Difficulty)1000);
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(
+            e => e.ShouldHaveSingleItem(),
+            e => e.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesExceptions.DIFFICULTY_LEVEL_NOT_SUPPORTED))
+            );
+    }
+
+    [Fact]
+    public void Error_Invalid_DishTypes()
+    {
+        var request = RequestFilterRecipeJsonBuilder.Build();
+        request.DishTypes.Add((MyRecipeBook.Communication.Enums.DishType)1000);
+
+        var validator = new FilterRecipeValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(
+            e => e.ShouldHaveSingleItem(),
+            e => e.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesExceptions.DISH_TYPE_NOT_SUPPORTED))
+            );
+    }
+}
