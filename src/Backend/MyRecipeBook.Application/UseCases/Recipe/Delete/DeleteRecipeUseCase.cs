a54bb163b249f0b1b96ba49ae4cbd95065217f38
Caio Robert <caio.robert@live.com>
2025-07-06 15:45:40 -0300

    feat: regra de deletar uma receita

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipe/Delete/DeleteRecipeUseCase.cs
@@ -0,0 +1,44 @@
+﻿using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.Recipe;
+using MyRecipeBook.Domain.Services.LoggedUser;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+
+namespace MyRecipeBook.Application.UseCases.Recipe.Delete;
+public class DeleteRecipeUseCase : IDeleteRecipeUseCase
+{
+    private readonly ILoggedUser _loggedUser;
+    private readonly IRecipeReadOnlyRepository _repositoryRead;
+    private readonly IRecipeWriteOnlyRepository _repositoryWrite;
+    private readonly IUnitOfWork _unitOfWork;
+    //private readonly IBlobStorageService _blobStorageService;
+
+    public DeleteRecipeUseCase(
+        ILoggedUser loggedUser,
+        IRecipeReadOnlyRepository repositoryRead,
+        IRecipeWriteOnlyRepository repositoryWrite,
+        IUnitOfWork unitOfWork)
+    {
+        _loggedUser = loggedUser;
+        _repositoryRead = repositoryRead;
+        _repositoryWrite = repositoryWrite;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Execute(long recipeId)
+    {
+        var loggedUser = await _loggedUser.User();
+
+        var recipe = await _repositoryRead.GetById(loggedUser, recipeId);
+
+        if (recipe is null)
+            throw new NotFoundException(ResourceMessagesExceptions.RECIPE_NOT_FOUND);
+
+        //if (recipe.ImageIdentifier.NotEmpty())
+        //    await _blobStorageService.Delete(loggedUser, recipe.ImageIdentifier);
+
+        await _repositoryWrite.Delete(recipeId);
+
+        await _unitOfWork.Commit();
+    }
+}
