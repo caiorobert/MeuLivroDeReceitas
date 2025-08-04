181eba044488d631fc140c8b15cb3dc61b17cec1
Caio Robert <caio.robert@live.com>
2025-08-03 21:37:50 -0300

    test: testes de integração com chatGPT

---
+++ tests/UseCases.Test/Recipe/Generate/GenerateRecipeUseCaseTest.cs
@@ -0,0 +1,54 @@
+﻿using CommonTestUtilities.Dtos;
+using CommonTestUtilities.OpenAI;
+using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.Recipe.Generate;
+using MyRecipeBook.Domain.Dtos;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+using Shouldly;
+using Xunit;
+
+namespace UseCases.Test.Recipe.Generate;
+public class GenerateRecipeUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var dto = GeneratedRecipeDtoBuilder.Build();
+
+        var request = RequestGenerateRecipeJsonBuilder.Build();
+
+        var useCase = CreateUseCase(dto);
+
+        var result = await useCase.Execute(request);
+
+        result.ShouldNotBeNull();
+        result.Title.ShouldBe(dto.Title);
+        result.CookingTime.ShouldBe((MyRecipeBook.Communication.Enums.CookingTime)dto.CookingTime);
+        result.Difficulty.ShouldBe(MyRecipeBook.Communication.Enums.Difficulty.Low);
+    }
+
+    [Fact]
+    public async Task Error_Duplicated_Ingredients()
+    {
+        var dto = GeneratedRecipeDtoBuilder.Build();
+
+        var request = RequestGenerateRecipeJsonBuilder.Build(count: 4);
+        request.Ingredients.Add(request.Ingredients[0]);
+
+        var useCase = CreateUseCase(dto);
+
+        var exception = await Should.ThrowAsync<ErrorOnValidationException>(() => useCase.Execute(request));
+        exception.ShouldNotBeNull();
+        var errors = exception.GetErrorMessages();
+        errors.Count.ShouldBe(1);
+        errors.ShouldContain(ResourceMessagesExceptions.DUPLICATED_INGREDIENTS_IN_LIST);
+    }
+
+    private static GenerateRecipeUseCase CreateUseCase(GeneratedRecipeDto dto)
+    {
+        var generateRecipeAI = GenerateRecipeAIBuilder.Build(dto);
+
+        return new GenerateRecipeUseCase(generateRecipeAI);
+    }
+}
---
+++ tests/Validators.Test/Recipe/Generate/GenerateRecipeValidatorTest.cs
@@ -0,0 +1,98 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.Recipe.Generate;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Helpers.Constants;
+using Shouldly;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Validators.Test.Recipe.Generate;
+public class GenerateRecipeValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new GenerateRecipeValidator();
+
+        var request = RequestGenerateRecipeJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Error_More_Maximum_Ingredient()
+    {
+        var validator = new GenerateRecipeValidator();
+
+        var request = RequestGenerateRecipeJsonBuilder
+            .Build(count: MyRecipeBookRuleConstants.MAXIMUM_INGREDIENTS_GENERATE_RECIPE + 1);
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(e =>
+        {
+            e.Count.ShouldBe(1);
+            e.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesExceptions.INVALID_NUMBER_INGREDIENTS));
+        });
+    }
+
+    [Fact]
+    public void Error_Duplicated_Ingredient()
+    {
+        var validator = new GenerateRecipeValidator();
+
+        var request = RequestGenerateRecipeJsonBuilder.Build(count: MyRecipeBookRuleConstants.MAXIMUM_INGREDIENTS_GENERATE_RECIPE - 1);
+        request.Ingredients.Add(request.Ingredients[0]);
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(e =>
+        {
+            e.Count.ShouldBe(1);
+            e.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesExceptions.DUPLICATED_INGREDIENTS_IN_LIST));
+        });
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("          ")]
+    [InlineData("")]
+    [SuppressMessage("Usage", "xUnit1012:Null should only be used for nullable parameters", Justification = "Because it is a unit test")]
+    public void Error_Empty_Ingredient(string ingredient)
+    {
+        var validator = new GenerateRecipeValidator();
+
+        var request = RequestGenerateRecipeJsonBuilder.Build(count: 1);
+        request.Ingredients.Add(ingredient);
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(e =>
+        {
+            e.Count.ShouldBe(1);
+            e.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesExceptions.INGREDIENT_EMPTY));
+        });
+    }
+
+    [Fact]
+    public void Error_Ingredient_Not_Following_Pattern()
+    {
+        var validator = new GenerateRecipeValidator();
+
+        var request = RequestGenerateRecipeJsonBuilder.Build(count: MyRecipeBookRuleConstants.MAXIMUM_INGREDIENTS_GENERATE_RECIPE - 1);
+        request.Ingredients.Add("This is an invalid ingredient because is too long");
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(e =>
+        {
+            e.Count.ShouldBe(1);
+            e.ShouldContain(e => e.ErrorMessage.Equals(ResourceMessagesExceptions.INGREDIENT_NOT_FOLLOWING_PATTERN));
+        });
+    }
+}
